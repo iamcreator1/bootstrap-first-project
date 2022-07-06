32995c7a9e2ab2c8a8b0a89770fbf3648863ffd1
Dmytro <104574268+iamcreator1@users.noreply.github.com>
2022-07-06 23:49:56 +0300

    add carousel to main banner

---
+++ js/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+
+  $('.owl-carousel').owlCarousel({
+    autoplay: true,
+    items: 1,
+    center: true,
+    autoplaySpeed: 1200,
+    loop: true,
+    margin: 0,
+    nav: false,
+    dots: false,
+    navText: ['', ' '],
+  });
+});
